d8d0bc01c485e927dbc0afa457af75bed591b9a3
jeong <kone31964@gmail.com>
2022-01-28 17:23:40 +0900

    22.01.28 HACCP

--- sever.js
+++ sever.js
@@ -71,3 +71,6 @@ app.get('/management', function (req, res){
 app.get('/upbringing_analysis', function (req, res){
     res.render('upbringing_analysis.html')
 });
+app.get('/haccp_monitoring', function (req, res){
+    res.render('haccp_monitoring.html')
+});
